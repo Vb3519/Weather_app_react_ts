9e6d9966244628a853ae3229e7e10743e3779918
vb3519 <vb415@bk.ru>
2025-05-19 23:55:17 +0300

    компонент с 5-ти дневным прогнозом погоды

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,8 @@ import { FaWind } from 'react-icons/fa';
 import { FaDroplet } from 'react-icons/fa6';
 import { MdVisibility } from 'react-icons/md';
 
+import { FaCaretDown } from 'react-icons/fa';
+
 const App = () => {
   return (
     <div className="h-screen w-full p-2 flex flex-col gap-4 items-center justify-center bg-gradient-135">
@@ -30,6 +32,7 @@ const App = () => {
       {/* --------------------------- MAIN: --------------------------- */}
       {/* --------------------------- MAIN: --------------------------- */}
       <main className="mb-auto w-full font-[inter] flex flex-col gap-4">
+        {/* --------------------------- Текущая погода: --------------------------- */}
         <section className="p-2 text-[#e0e0e0] text-shadow-sm flex flex-col gap-1 bg-white/20 rounded-sm">
           <h2 className="font-semibold text-xl text-center">Санкт-Петербург</h2>
           <p>
@@ -71,6 +74,7 @@ const App = () => {
           </ul>
         </section>
 
+        {/* --------------------------- Погода на ближайшие 12 часов: --------------------------- */}
         <section className="p-2 text-[#e0e0e0] text-sm text-shadow-sm bg-white/20 rounded-sm">
           <ul className="flex gap-1 items-center justify-between">
             <li className="p-1 flex flex-col items-center justify-center gap-1 cursor-pointer rounded-sm transition duration-200 ease-in hover:bg-white/15 hover:[box-shadow:0_4px_10px_rgba(0,0,0,0.15)]">
@@ -112,6 +116,85 @@ const App = () => {
                 alt="weather state"
               ></img>
               <p>11.15°С</p>
+            </li>
+          </ul>
+        </section>
+
+        {/* --------------------------- Прогноз погоды на 5 дней (общий): --------------------------- */}
+        <section className="p-2 text-[#e0e0e0] text-sm text-shadow-sm bg-white/20 rounded-sm">
+          <h2 className="pb-2 font-semibold text-xl text-center border-b-2 border-b-blue-800/20">
+            Прогноз погоды на 5 дней:
+          </h2>
+          <ul className="mt-2 flex flex-col gap-2">
+            <li className="px-2 flex items-center gap-2 cursor-pointer rounded-sm transition duration-200 ease-in hover:bg-white/15 hover:[box-shadow:0_4px_10px_rgba(0,0,0,0.15)]">
+              <span>Пн</span>
+              <div>
+                <img
+                  src="http://openweathermap.org/img/wn/10d.png"
+                  alt="weather 5-day forecast state"
+                ></img>
+              </div>
+              <span>12°С</span>
+              <span className="ml-auto text-center">Небольшой дождь</span>
+              <div>
+                <FaCaretDown />
+              </div>
+            </li>
+            <li className="px-2 flex items-center gap-2 cursor-pointer rounded-sm transition duration-200 ease-in hover:bg-white/15 hover:[box-shadow:0_4px_10px_rgba(0,0,0,0.15)]">
+              <span>Вт</span>
+              <div>
+                <img
+                  src="http://openweathermap.org/img/wn/02d.png"
+                  alt="weather 5-day forecast state"
+                ></img>
+              </div>
+              <span>12°С</span>
+              <span className="ml-auto text-center">Небольшая облачность</span>
+              <div>
+                <FaCaretDown />
+              </div>
+            </li>
+            <li className="px-2 flex items-center gap-2 cursor-pointer rounded-sm transition duration-200 ease-in hover:bg-white/15 hover:[box-shadow:0_4px_10px_rgba(0,0,0,0.15)]">
+              <span>Ср</span>
+              <div>
+                <img
+                  src="http://openweathermap.org/img/wn/10d.png"
+                  alt="weather 5-day forecast state"
+                ></img>
+              </div>
+              <span>12°С</span>
+              <span className="ml-auto text-center">Небольшой дождь</span>
+              <div>
+                <FaCaretDown />
+              </div>
+            </li>
+            <li className="px-2 flex items-center gap-2 cursor-pointer rounded-sm transition duration-200 ease-in hover:bg-white/15 hover:[box-shadow:0_4px_10px_rgba(0,0,0,0.15)]">
+              <span>Чт</span>
+              <div>
+                <img
+                  src="http://openweathermap.org/img/wn/02d.png"
+                  alt="weather 5-day forecast state"
+                ></img>
+              </div>
+              <span>12°С</span>
+              <span className="ml-auto text-center">Небольшая облачность</span>
+              <div>
+                <FaCaretDown />
+              </div>
+            </li>
+            <li className="px-2 flex items-center gap-2 cursor-pointer rounded-sm transition duration-200 ease-in hover:bg-white/15 hover:[box-shadow:0_4px_10px_rgba(0,0,0,0.15)]">
+              <span>Пт</span>
+              <div>
+                <img
+                  src="http://openweathermap.org/img/wn/10d.png"
+                  alt="weather 5-day forecast state"
+                ></img>
+              </div>
+              <span>12°С</span>
+              <span className="ml-auto text-center">Небольшой дождь</span>
+              <div>
+                <FaCaretDown />
+              </div>
             </li>
           </ul>
         </section>
