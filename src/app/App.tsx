ecfd321b50f0fb6496f972306f57365728e6bac1
vb3519 <vb415@bk.ru>
2025-05-25 22:17:37 +0300

    верстка под небольшие экраны mobile first готова

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -305,21 +305,27 @@ const App = () => {
       {/* --------------------------- FOOTER: --------------------------- */}
       {/* --------------------------- FOOTER: --------------------------- */}
       {/* --------------------------- FOOTER: --------------------------- */}
-      <footer className="p-2 h-40 w-full text-[#e0e0e0] text-shadow-sm footer-bg-gradient">
-        <h3 className="font-semibold">Weather App</h3>
-        <ul className="flex gap-2">
-          <li className="p-2 rounded-[50%] bg-green-700 cursor-pointer">
-            <FaWhatsapp className="text-2xl" />
-          </li>
-          <li className="p-2 rounded-[50%] bg-blue-600 cursor-pointer">
-            <FaTelegramPlane className="text-2xl" />
-          </li>
-        </ul>
-        <div className="flex gap-2 items-center justify-start">
-          <MdOutlineEmail />
-          <span>vb415@bk.ru</span>
+      <footer className="p-2 w-full flex flex-col gap-2 text-[#e0e0e0] text-shadow-sm footer-bg-gradient">
+        <h3 className="mb-2 font-semibold text-xl text-[#e0e0e0]">
+          Weather App
+        </h3>
+        <div className="flex flex-col gap-2">
+          <ul className="flex gap-2">
+            <li className="p-2 rounded-[50%] bg-green-700 cursor-pointer">
+              <FaWhatsapp className="text-2xl" />
+            </li>
+            <li className="p-2 rounded-[50%] bg-blue-600 cursor-pointer">
+              <FaTelegramPlane className="text-2xl" />
+            </li>
+          </ul>
+          <div className="flex gap-2 items-center justify-start">
+            <MdOutlineEmail className="text-2xl" />
+            <span className="font-semibold">vb415@bk.ru</span>
+          </div>
         </div>
-        <span>by Viktor Bordyugov 2025</span>
+        <span className="font-semibold text-blue-200/20">
+          by Viktor Bordyugov 2025
+        </span>
       </footer>
     </div>
   );
