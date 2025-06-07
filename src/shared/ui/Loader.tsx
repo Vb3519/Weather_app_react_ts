ae814554d9d98470baeb9064a962e2bda59bfec9
vb3519 <vb415@bk.ru>
2025-06-07 23:35:26 +0300

    лоадер и анимации загрузки для компонентов

---
+++ src/shared/ui/Loader.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+const Loader = () => {
+  const loaderDots: string = '...';
+
+  const renderLoaderDots = async () => {};
+
+  return (
+    <div>
+      <h2>Загрузка</h2>
+    </div>
+  );
+};
